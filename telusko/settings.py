0b7930e96d9677d65b569165a2d385c0a56b8c7c
bharath <bsimhareddyls1@gmail.com>
2021-07-20 02:09:37 +0530

    loaded images and css

--- telusko/settings.py
+++ telusko/settings.py
@@ -122,9 +122,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("text/html", ".html", True)
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
